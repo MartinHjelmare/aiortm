4eee1dfee7d135dc2ddb72b6a29d43bdf53ca6a1
Martin Hjelmare <marhje52@gmail.com>
2023-11-05 17:54:22 +0100

    feat: export common names

--- src/aiortm/__init__.py
+++ src/aiortm/__init__.py
@@ -1,2 +1,25 @@
 """Use the Remember the Milk API with aiohttp."""
+from .client import AioRTMClient, Auth
+from .exceptions import (
+    AioRTMError,
+    APIAuthError,
+    APIResponseError,
+    AuthError,
+    ResponseError,
+    TransportAuthError,
+    TransportResponseError,
+)
+
 __version__ = "0.7.0"
+
+__all__ = [
+    "AioRTMClient",
+    "Auth",
+    "AioRTMError",
+    "APIAuthError",
+    "APIResponseError",
+    "AuthError",
+    "ResponseError",
+    "TransportAuthError",
+    "TransportResponseError",
+]
